c875080b523727bffbba7bba7f11cbf1d7cbc1fa
Jody W Davis <joddavis@cisco.com>
2022-01-06 16:41:11 -0600

    Update env_var.py

--- env_var.py
+++ env_var.py
@@ -1,8 +1,8 @@
 config = {}
 
-config['x_cisco_meraki_api_key'] = "581bc820001cda90558216baf4621b28c67c172f"
-config['webex_teams_acces_token'] = "YWRhZTRiNTQtNjc0NS00ZTE2LWE2ZWItNGU4ODc4YmE0MTUzZDU5YTU0OGQtNDhh_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
-config['webex_room_id'] = "Y2lzY29zcGFyazovL3VzL1JPT00vN2M0MWI2MDAtZjkzYi0xMWViLTg1MTEtNDMxYjM3NTIyOTY0"
+config['x_cisco_meraki_api_key'] = ""
+config['webex_teams_acces_token'] = ""
+config['webex_room_id'] = ""
 config['device_name_filter'] = "MS120-8"
 config['device_username'] = "python"
 config['device_password'] = "python"
